9fcb88384def243025c6d96dbc3cf41c7f18a1e1
kinaoshi <kinaoshimokkou@yahoo.co.jp>
2021-01-25 14:16:11 +0900

    1/25_2

--- js/main2.js
+++ js/main2.js
@@ -26,17 +26,24 @@
   const page2 = document.querySelector(".page2");
 
   function started() {
-    const nums1 = [5, 6, 7, 8, 9];
-    let numcou1 = nums1[Math.floor(Math.random() * nums1.length)];
+    let min1 = 3;
+    let max1 = 9;
+    const numcou1 = Math.floor(Math.random() * (max1 - min1 + 1) + min1);
     num1.textContent = numcou1;
-    const nums2 = [1, 2, 3, 4];
-    let numcou2 = nums2[Math.floor(Math.random() * nums2.length)];
+
+    let min2 = 4;
+    let max2 = 9;
+    const numcou2 = Math.floor(Math.random() * (max2 - min2 + 1) + min2);
     num2.textContent = numcou2;
-    const nums3 = [1, 2, 4];
-    let numcou3 = nums3[Math.floor(Math.random() * nums3.length)];
+    let min3 = 3;
+    let max3 = 9;
+
+    const numcou3 = Math.floor(Math.random() * (max3 - min3 + 1) + min3);
     num3.textContent = numcou3;
-    const nums4 = [3, 5];
-    let numcou4 = nums4[Math.floor(Math.random() * nums4.length)];
+    let min4 = 3;
+    let max4 = 9;
+
+    const numcou4 = Math.floor(Math.random() * (max4 - min4 + 1) + min4);
     num4.textContent = numcou4;
 
     ansnum1.textContent = "";
@@ -56,9 +63,8 @@
     prev5.classList.add("eras");
     next.classList.remove("eras");
     page1.style.display = "none";
-    page2.classList.remove('hidden');
+    page2.classList.remove("hidden");
 
-    
     if (
       start.textContent === "スタート" ||
       coun.textContent === "1" ||
@@ -81,123 +87,107 @@
   const coun = document.querySelector(".coun");
   let qes = document.querySelector(".qes");
 
-  check.addEventListener("click", () => {
-    let numcou1 = parseInt(num1.textContent);
-    let numcou2 = parseInt(num2.textContent);
-    let numcou3 = parseInt(num3.textContent);
-    let numcou4 = parseInt(num4.textContent);
-    let red = 2;
-    let blue = 1;
-
-    if (coun.textContent === "1" || "2" || "3" || "4" || "5") {
-      if (coun.textContent === "1") {
-        if (
-          numcou1 + numcou2 ===
-          parseInt(ansnum1.textContent * 10) + parseInt(ansnum2.textContent)
-        ) {
-          // console.log(numcou1 + numcou2);
-          correct.classList.remove("hidden");
-          mistake.classList.add("hidden");
-        } else {
-          correct.classList.add("hidden");
-          mistake.classList.remove("hidden");
-        }
-      }
-
-      if (coun.textContent === "2") {
-        if (
-          numcou3 + numcou4 ===
-          parseInt(ansnum1.textContent * 10) + parseInt(ansnum2.textContent)
-        ) {
-          correct.classList.remove("hidden");
-          mistake.classList.add("hidden");
-        } else {
-          correct.classList.add("hidden");
-          mistake.classList.remove("hidden");
-        }
-      }
-
-      if (coun.textContent === "3") {
-        if (
-          numcou1 + numcou2 + numcou3 + numcou4 ===
-          parseInt(ansnum1.textContent * 10) + parseInt(ansnum2.textContent)
-        ) {
-          correct.classList.remove("hidden");
-          mistake.classList.add("hidden");
-        } else {
-          correct.classList.add("hidden");
-          mistake.classList.remove("hidden");
-        }
-      }
-
-      if (coun.textContent === "4") {
-        if (
-          numcou1 + numcou3 - red ===
-          parseInt(ansnum1.textContent * 10) + parseInt(ansnum2.textContent)
-        ) {
-          correct.classList.remove("hidden");
-          mistake.classList.add("hidden");
-        } else {
-          correct.classList.add("hidden");
-          mistake.classList.remove("hidden");
-        }
-      }
-
-      if (coun.textContent === "5") {
-        if (
-          numcou1 + numcou2 + numcou3 + numcou4 - red - blue ===
-          parseInt(ansnum1.textContent * 10) + parseInt(ansnum2.textContent)
-        ) {
-          correct.classList.remove("hidden");
-          mistake.classList.add("hidden");
-        } else {
-          correct.classList.add("hidden");
-          mistake.classList.remove("hidden");
-        }
-      }
-    }
-  });
-
   const qes2 = document.querySelector(".qes2");
   const qes3 = document.querySelector(".qes3");
   const qes4 = document.querySelector(".qes4");
   const qes5 = document.querySelector(".qes5");
 
-  next.addEventListener("click", () => {
-    ansnum1.textContent = "";
-    ansnum2.textContent = "";
+  let qes2bus = document.querySelector(".qes2bus");
+  let qes2tra = document.querySelector(".qes2tra");
+  let qes3pc = document.querySelector(".qes3pc");
+  let qes3fiar = document.querySelector(".qes3fiar");
 
-    correct.classList.add("hidden");
-    mistake.classList.add("hidden");
+  next.addEventListener("click", () => {
+    let numcou1 = parseInt(num1.textContent);
+    let numcou2 = parseInt(num2.textContent);
+    let numcou3 = parseInt(num3.textContent);
+    let numcou4 = parseInt(num4.textContent);
 
     if (coun.textContent === "1") {
       coun.textContent = "2";
+
+      let min1 = 1;
+      let max1 = 6;
+      const qes2bus1 = Math.floor(Math.random() * (max1 - min1 + 1) + min1);
+
+      let min2 = 1;
+      let max2 = 7;
+
+      const qes2tra1 = Math.floor(Math.random() * (max2 - min2 + 1) + min2);
+
+      if (numcou1 < qes2bus1) {
+        qes2bus.textContent = numcou1;
+      } else {
+        qes2bus.textContent = qes2bus1;
+      }
+      if (numcou2 < qes2tra1) {
+        qes2tra.textContent = numcou2;
+      } else {
+        qes2tra.textContent = qes2tra1;
+      }
+
+      ansnum1.textContent = "";
+      ansnum2.textContent = "";
+
+      correct.classList.add("hidden");
+      mistake.classList.add("hidden");
+
       qes.style.display = "none";
       qes2.style.display = "block";
       prev2.classList.remove("eras");
-      ansnum1.textContent = "";
-      ansnum2.textContent = "";
-      
     } else if (coun.textContent === "2") {
       coun.textContent = "3";
+      correct.classList.add("hidden");
+      mistake.classList.add("hidden");
+
+      prev3.classList.remove("eras");
+
       qes2.style.display = "none";
       qes3.style.display = "block";
-      prev3.classList.remove("eras");
+
+      let min1 = 1;
+      let max1 = 6;
+      const qes3pc1 = Math.floor(Math.random() * (max1 - min1 + 1) + min1);
+
+      let min2 = 1;
+      let max2 = 7;
+      const qes3fiar1 = Math.floor(Math.random() * (max2 - min2 + 1) + min2);
+
+      if (numcou3 < qes3pc1) {
+        qes3pc.textContent = numcou3;
+      } else {
+        qes3pc.textContent = qes3pc1;
+      }
+      if (numcou4 < qes3fiar1) {
+        qes3fiar.textContent = numcou4;
+      } else {
+        qes3fiar.textContent = qes3fiar1;
+      }
+
+
       ansnum1.textContent = "";
       ansnum2.textContent = "";
     } else if (coun.textContent === "3") {
       coun.textContent = "4";
+      prev4.classList.remove("eras");
       qes3.style.display = "none";
       qes4.style.display = "block";
-      prev4.classList.remove("eras");
       ansnum1.textContent = "";
       ansnum2.textContent = "";
+      correct.classList.add("hidden");
+      mistake.classList.add("hidden");
     } else if (coun.textContent === "4") {
       coun.textContent = "5";
+
+      prev5.classList.remove("eras");
       qes4.style.display = "none";
       qes5.style.display = "block";
-      prev5.classList.remove("eras");
       next.classList.add("eras");
+      ansnum1.textContent = "";
+      ansnum2.textContent = "";
+      correct.classList.add("hidden");
+      mistake.classList.add("hidden");
+      
     }
   });
 
@@ -220,7 +210,6 @@
     mistake.classList.add("hidden");
     // prev1.classList.add("eras");
     // prev2.classList.remove("eras");
-
   });
   prev2.addEventListener("click", () => {
     coun.textContent = "2";
@@ -274,6 +263,108 @@
     ansnum1.textContent = "";
     correct.classList.add("hidden");
     mistake.classList.add("hidden");
+  });
+
+  check.addEventListener("click", () => {
+    let numcou1 = parseInt(num1.textContent);
+    let numcou2 = parseInt(num2.textContent);
+    let numcou3 = parseInt(num3.textContent);
+    let numcou4 = parseInt(num4.textContent);
+    let qes2busis = parseInt(qes2bus.textContent);
+    let qes2trais = parseInt(qes2tra.textContent);
+    let qes3pcis = parseInt(qes3pc.textContent);
+    let qes3fiaris = parseInt(qes3fiar.textContent);
+    // console.log(qes3fiaris);
+
+    if (coun.textContent === "1" || "2" || "3" || "4" || "5") {
+      if (coun.textContent === "1") {
+        if (
+          numcou1 + numcou2 + numcou3 + numcou4 ===
+          parseInt(ansnum1.textContent * 10) + parseInt(ansnum2.textContent)
+        ) {
+          console.log(numcou1 + numcou2 + numcou3 + numcou4);
+
+          correct.classList.remove("hidden");
+          mistake.classList.add("hidden");
+        } else {
+          correct.classList.add("hidden");
+          mistake.classList.remove("hidden");
+        }
+      }
+
+      if (coun.textContent === "2") {
+        if (
+          numcou1 + numcou2 + numcou3 + numcou4 - qes2busis - qes2trais ===
+          parseInt(ansnum1.textContent * 10) + parseInt(ansnum2.textContent)
+        ) {
+          console.log(
+            numcou1 + numcou2 + numcou3 + numcou4 - qes2busis - qes2trais
+          );
+          correct.classList.remove("hidden");
+          mistake.classList.add("hidden");
+        } else {
+          correct.classList.add("hidden");
+          mistake.classList.remove("hidden");
+        }
+      }
+
+      if (coun.textContent === "3") {
+        if (
+          numcou1 +
+            numcou2 +
+            numcou3 +
+            numcou4 -
+            qes2busis -
+            qes2trais -
+            qes3pcis -
+            qes3fiaris ===
+          parseInt(ansnum1.textContent * 10) + parseInt(ansnum2.textContent)
+        ) {
+          correct.classList.remove("hidden");
+          mistake.classList.add("hidden");
+          console.log(
+            numcou1 +
+              numcou2 +
+              numcou3 +
+              numcou4 -
+              qes2busis -
+              qes2trais -
+              qes3pcis -
+              qes3fiaris
+          );
+        } else {
+          correct.classList.add("hidden");
+          mistake.classList.remove("hidden");
+        }
+      }
+
+      if (coun.textContent === "4") {
+        if (
+          numcou1 + numcou3 - qes2busis - qes3pcis ===
+          parseInt(ansnum1.textContent * 10) + parseInt(ansnum2.textContent)
+        ) {
+          correct.classList.remove("hidden");
+          mistake.classList.add("hidden");
+          console.log(qes2busis + qes3pcis);
+        } else {
+          correct.classList.add("hidden");
+          mistake.classList.remove("hidden");
+        }
+      }
+
+      if (coun.textContent === "5") {
+        if (
+          numcou2 + numcou4 - qes2trais - qes3fiaris ===
+          parseInt(ansnum1.textContent * 10) + parseInt(ansnum2.textContent)
+        ) {
+          correct.classList.remove("hidden");
+          mistake.classList.add("hidden");
+        } else {
+          correct.classList.add("hidden");
+          mistake.classList.remove("hidden");
+        }
+      }
+    }
   });
 
   box1.addEventListener("click", () => {
@@ -358,8 +449,8 @@
     }
   });
   clear2.addEventListener("click", () => {
-    ansnum2.textContent = "　";
-    ansnum1.textContent = "　";
+    ansnum2.textContent = "";
+    ansnum1.textContent = "";
     correct.classList.add("hidden");
     mistake.classList.add("hidden");
   });
